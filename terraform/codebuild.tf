5b7f136bf101abbb882ea051811745a56a891eb1
scg <golights@purplellama.net>
2020-07-27 00:25:22 +0100

    Add codebuild, with webhook to github
    - triggered by commit to github project
    - builds docker image
    - pushes  image into ECR project

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -5,22 +5,141 @@ resource "aws_codebuild_source_credential" "tech-test" {
 }
 
 resource "aws_codebuild_webhook" "tech-test" {
-  project_name = var.aws_codebuild_project.example.name
+  project_name = aws_codebuild_project.tech-test.name
+}
 
-  filter_group {
-    filter {
-      type    = "EVENT"
-      pattern = "PUSH"
+resource "aws_iam_role" "tech-test" {
+  name = "tf-${var.image_name}"
+
+  assume_role_policy = <<ROLE
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codebuild.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+ROLE
+}
+
+resource "aws_iam_role_policy" "tech-test" {
+  role = aws_iam_role.tech-test.name
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "codebuild:BatchPutTestCases",
+                "codebuild:CreateReport",
+                "codebuild:CreateReportGroup",
+                "codebuild:UpdateReport"
+            ],
+            "Resource": "arn:aws:codebuild:*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:BatchGetImage",
+                "ecr:CompleteLayerUpload",
+                "ecr:DescribeImages",
+                "ecr:DescribeImageScanFindings",
+                "ecr:DescribeRepositories",
+                "ecr:GetAuthorizationToken",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:GetLifecyclePolicy",
+                "ecr:GetLifecyclePolicyPreview",
+                "ecr:GetRepositoryPolicy",
+                "ecr:InitiateLayerUpload",
+                "ecr:ListImages",
+                "ecr:ListTagsForResource",
+                "ecr:PutImage",
+                "ecr:UploadLayerPart"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetBucketAcl",
+                "s3:GetBucketLocation",
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:PutObject"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_codebuild_project" "tech-test" {
+  name          = "tf-${var.image_name}"
+  build_timeout = "5"
+  service_role  = aws_iam_role.tech-test.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:4.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name  = "IMAGE_NAME"
+      value = var.image_name
     }
 
-    filter {
-      type    = "HEAD_REF"
-      pattern = "master"
+    environment_variable {
+      name  = "IMAGE_TAG"
+      value = var.image_tag
+    }
+
+    environment_variable {
+      name  = "AWS_REGION"
+      value = var.aws_region
+    }
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = var.aws_account_id
     }
   }
-}
 
-resource "aws_iam_service_linked_role" "codebuild" {
-  aws_service_name = "codebuild.amazonaws.com"
-}
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "log-group"
+      stream_name = "log-stream"
+    }
+  }
 
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/golights/${var.image_name}"
+    git_clone_depth = 1
+  }
+
+  source_version = "master"
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -2,9 +2,16 @@ variable "aws_region" {
   default = "eu-west-2"
 }
 
+variable "aws_account_id" {
+  default = "642981871556"
+}
+
 variable "image_name" {
 }
 
-variable "github_access_token" {
+variable "image_tag" {
+  default = "latest"
+}
 
+variable "github_access_token" {
 }
